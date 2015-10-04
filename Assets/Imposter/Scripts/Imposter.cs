222f4d7b268d581236f42a99440a401a53dfd268
Krystian Babilinski <kbabilinski@gmail.com>
2015-10-03 20:00:19 -0400

    Initial Push _2
    Git didn't grab all the files

---
+++ Assets/Imposter/Scripts/Imposter.cs
@@ -0,0 +1,252 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Imposter : MonoBehaviour 
+{
+    public enum ImposterLodMethod
+    {
+        Distance, ScreenSize
+    }
+
+    public int maxTextureSize = 256;
+    [HideInInspector]
+    public ImposterLodMethod lodMethod = ImposterLodMethod.ScreenSize;
+    public float maxDistance = 30.0f;
+    public float angleTolerance = 2.5f;
+    public float distanceTolerance = 15.0f;
+    public float zOffset = 0.25f;
+    public bool castShadow = false;
+    public float shadowZOffset = 0.25f;
+    public float maxShadowDistance = 1.0f;
+    public int shadowDownSampling = 2;
+    
+    private float pixelSize = 0.0f;
+    private int lastTextureSize = 0;
+    private ImposterProxy proxy;
+    private Vector3 lastCameraVector = Vector3.zero;
+    private List<Renderer> renderers = new List<Renderer>();
+    private Dictionary<Renderer, int> originalRenderLayers = new Dictionary<Renderer, int>();
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (ImposterManager.instance == null)
+        {
+            Debug.LogError("Can´t find ImposterManager!");
+            return;
+        }
+
+        Init();
+	}
+
+    public void Init()
+    {
+        lastCameraVector = transform.position - ImposterManager.instance.mainCamera.transform.position;
+        if (!ImposterManager.instance.imposters.Contains(this))
+        {
+            ImposterManager.instance.imposters.Add(this);
+        }
+
+        if (proxy != null)
+        {
+            DestroyImmediate(proxy.gameObject);
+        }
+
+        extractRenderer();
+        createProxy();
+        deactivate();
+    }
+
+    private void extractRenderer()
+    {
+        renderers.Clear();
+
+        if (GetComponent<Renderer>() != null)
+        {
+            renderers.Add(GetComponent<Renderer>());
+        }
+
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            renderers.Add(r);
+        }
+
+        foreach (Renderer r in renderers)
+        {
+            if (!originalRenderLayers.ContainsKey(r))
+            {
+                originalRenderLayers.Add(r, r.gameObject.layer);
+            }
+        }
+    }
+
+    private void createProxy()
+    {
+        GameObject proxyGo = (GameObject)Instantiate(ImposterManager.instance.proxyPrefab.gameObject, transform.position, transform.rotation);
+        proxyGo.transform.SetParent(transform);
+        proxyGo.transform.localScale = new Vector3(1.0f / transform.lossyScale.x, 1.0f / transform.lossyScale.y, 1.0f / transform.lossyScale.z);
+        
+        proxyGo.SetActive(true);
+        proxy = proxyGo.GetComponent<ImposterProxy>();
+        proxy.shadowZOffset = shadowZOffset;
+        proxy.zOffset = zOffset;
+        proxy.shadowDivider = 1 << shadowDownSampling;
+        proxy.maxShadowDistance = maxShadowDistance;
+        proxy.Init(renderers, castShadow && ImposterManager.instance.castShadow);
+    }
+
+    private void activate()
+    {
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = false;
+            r.gameObject.layer = ImposterManager.instance.imposterLayer;
+        }
+
+        proxy.gameObject.SetActive(true);
+    }
+
+    private void deactivate()
+    {
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = true;
+
+            int originalLayer = 0;
+            originalRenderLayers.TryGetValue(r, out originalLayer);
+            r.gameObject.layer = originalLayer;
+        }
+
+        proxy.gameObject.SetActive(false);
+    }
+
+	public void UpdateImposter ()
+    {
+        if (!ImposterManager.instance.active)
+        {
+            if (proxy.isActiveAndEnabled)
+            {
+                proxy.InvalidateTexture();
+                deactivate();
+            }
+            return;
+        }
+
+        Vector3 cameraVector = transform.position - ImposterManager.instance.mainCamera.transform.position;
+        bool isInRange = IsInRange();
+        bool isInFrustrum = proxy.isVisible();
+        bool needsUpdate = NeedsUpdate();
+
+        if (isInRange)
+        {
+            if (!proxy.isActiveAndEnabled && ImposterManager.instance.active)
+            {
+                activate();
+            }
+
+            if (isInFrustrum)
+            {
+                proxy.setVisibility(true);
+
+                if (needsUpdate || proxy.IsTextureInvalid())
+                {
+                    fitTexture();
+                    proxy.Render();
+                    lastCameraVector = cameraVector;
+                }
+            }
+            else
+            {
+                bool isVisibleInCache = proxy.isVisibleInCache();
+
+                proxy.setVisibility(false);
+
+                if (isVisibleInCache && ImposterManager.instance.cachingBehaviour == ImposterManager.CachingBehaviour.preloadAndCacheInvisibleImposters)
+                {
+                    if (needsUpdate || proxy.IsTextureInvalid())
+                    {
+                        if(ImposterManager.instance.getPreloadLock(this))
+                        {
+                            fitTexture();
+                            proxy.Render();
+                            lastCameraVector = cameraVector;
+                        }
+                    }
+                }
+                else if (ImposterManager.instance.cachingBehaviour == ImposterManager.CachingBehaviour.discardInvisibleImpostors || needsUpdate || !isVisibleInCache)
+                {
+                    proxy.InvalidateTexture();
+                }
+            }
+        }
+        else
+        {
+            if (proxy.isActiveAndEnabled || !ImposterManager.instance.active && proxy.isActiveAndEnabled)
+            {
+                proxy.InvalidateTexture();
+                deactivate();
+                return;
+            }
+        }
+	}
+
+    private bool NeedsUpdate()
+    {
+        Vector3 cameraVector = transform.position - ImposterManager.instance.mainCamera.transform.position;
+        float diff = Vector3.Angle(cameraVector, lastCameraVector);
+        float distDiff = Mathf.Abs(cameraVector.magnitude - lastCameraVector.magnitude);
+
+        if (diff > angleTolerance || distDiff > lastCameraVector.magnitude * (distanceTolerance / 100.0f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool IsInRange()
+    {
+        bool isInRange = false;
+
+        float cameraDistance = Vector3.Distance(ImposterManager.instance.mainCamera.transform.position, transform.position);
+
+        if (lodMethod == ImposterLodMethod.Distance)
+        {
+            isInRange = cameraDistance > maxDistance;
+        }
+        else
+        {
+            float angularSize = (proxy.maxSize / cameraDistance) * Mathf.Rad2Deg;
+            pixelSize = ((angularSize * Screen.height) / ImposterManager.instance.mainCamera.fieldOfView);
+            isInRange = pixelSize < maxTextureSize;
+        }
+        return isInRange;
+    }
+
+    private void fitTexture()
+    {
+        int textureSize = Mathf.Min(maxTextureSize, nlpo2((int)pixelSize));
+
+        if (proxy.IsTextureInvalid() || lastTextureSize != textureSize)
+        {
+            proxy.AdjustTextureSize(textureSize);
+        }
+
+        lastTextureSize = textureSize;
+    }
+
+    private int nlpo2(int x)
+    {
+        x--;
+        x |= (x >> 1);
+        x |= (x >> 2);
+        x |= (x >> 4);
+        x |= (x >> 8);
+        x |= (x >> 16);
+
+        return (x+1);
+    }
+}
---
+++ Assets/Imposter/Scripts/ImposterManager.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ImposterManager : MonoBehaviour
+{
+    public enum CachingBehaviour { discardInvisibleImpostors, cacheInvisibleIposters, preloadAndCacheInvisibleImposters }
+
+    public static ImposterManager instance;
+
+    public bool active = true;
+    public int imposterLayer = 30;
+    public bool useMainCamera = true;
+    public Camera mainCamera;
+    public int antialiasing = 2;
+
+    public bool castShadow = false;
+    public Light mainLight;
+
+    public CachingBehaviour cachingBehaviour;
+    public float preLoadFovFactor = 2.0f;
+    public int preloadRate = 5;
+
+    public List<Imposter> imposters;
+    public List<ImposterTexture> imposterTextures = new List<ImposterTexture>();
+    public List<ImposterTexture> freeImposterTextures = new List<ImposterTexture>();
+    public float textureMemory = 0.0f;
+
+    public ImposterProxy proxyPrefab;
+    public Camera imposterRenderingCamera;
+
+    private Plane[] cameraPlanes, cachingCameraPlanes;
+    private bool lastCastShadow;
+    private List<Imposter> invalidImposters = new List<Imposter>();
+    private Camera cachingCamera;
+    private int preloadCounter = 0;
+
+    void Awake()
+    {
+        instance = this;
+        lastCastShadow = castShadow;
+
+        if (useMainCamera)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        SetupCachingCamera();
+	}
+
+    void SetupCachingCamera()
+    {
+        GameObject go = new GameObject("Imposter Caching Camera");
+        cachingCamera = go.AddComponent<Camera>();
+
+        cachingCamera.orthographic = ImposterManager.instance.mainCamera.orthographic;
+        cachingCamera.orthographicSize = ImposterManager.instance.mainCamera.orthographicSize;
+        cachingCamera.fieldOfView = ImposterManager.instance.mainCamera.fieldOfView * preLoadFovFactor;
+        cachingCamera.nearClipPlane = ImposterManager.instance.mainCamera.nearClipPlane;
+        cachingCamera.farClipPlane = ImposterManager.instance.mainCamera.farClipPlane;
+        cachingCamera.transform.position = ImposterManager.instance.mainCamera.transform.position;
+        cachingCamera.transform.rotation = ImposterManager.instance.mainCamera.transform.rotation;
+        cachingCamera.enabled = false;
+
+        go.transform.SetParent(ImposterManager.instance.mainCamera.transform);
+    }
+
+    void LateUpdate()
+    {
+        preloadCounter = 0;
+        cameraPlanes = GeometryUtility.CalculateFrustumPlanes(ImposterManager.instance.mainCamera);
+        cachingCameraPlanes = GeometryUtility.CalculateFrustumPlanes(cachingCamera);
+        castShadow = castShadow && mainLight != null && mainLight.enabled && mainLight.type == LightType.Directional && mainLight.shadows != LightShadows.None;
+
+        //Update Imposters
+        foreach (Imposter imposter in imposters)
+        {
+            if (imposter != null)
+            {
+                imposter.UpdateImposter();
+            }
+            else
+            {
+                invalidImposters.Add(imposter);
+            }
+        }
+
+        //Delete invalid Imposters
+        if (invalidImposters.Count > 0)
+        {
+            for (int i = 0; i < invalidImposters.Count; i++)
+            {
+                imposters.Remove(invalidImposters[i]);
+            }
+
+            invalidImposters.Clear();
+        }
+    }
+
+    public bool isVisible(Bounds bound)
+    {
+        return GeometryUtility.TestPlanesAABB(cameraPlanes, bound);
+    }
+
+    public bool isVisibleInCache(Bounds bound)
+    {
+        return GeometryUtility.TestPlanesAABB(cachingCameraPlanes, bound);
+    }
+
+    public ImposterTexture getRenderTexture(ImposterProxy newOwner, int size)
+    {
+        //Reuse Texture?
+        for(int i=0; i<freeImposterTextures.Count; i++)
+        {
+            ImposterTexture texture = freeImposterTextures[i];
+
+            if (texture.size == size)
+            {
+                freeImposterTextures.Remove(texture);
+                texture.owner = newOwner;
+
+                return texture;
+            }
+        }
+
+        //Create new Texture
+        RenderTexture renderTexture = new RenderTexture(size, size, 16);
+        renderTexture.antiAliasing = antialiasing;
+
+        ImposterTexture imposterTexture = new ImposterTexture();
+        imposterTexture.owner = newOwner;
+        imposterTexture.size = size;
+        imposterTexture.createdTime = imposterTexture.lastUsedTime = Time.frameCount;
+        imposterTexture.texture = renderTexture;
+        imposterTextures.Add(imposterTexture);
+
+        textureMemory += imposterTexture.getMemoryAmount();
+
+        return imposterTexture;
+    }
+
+    public void giveBackRenderTexture(ImposterTexture imposterTexture)
+    {
+        if (imposterTexture.texture == null)
+        {
+            Debug.LogError("Texture given back is empty!");
+            return;
+        }
+
+        freeImposterTextures.Add(imposterTexture);
+    }
+
+    public void garbageCollect()
+    {
+        foreach (ImposterTexture imposterTexture in freeImposterTextures)
+        {
+            imposterTextures.Remove(imposterTexture);
+            textureMemory -= imposterTexture.getMemoryAmount();
+            Destroy(imposterTexture.texture);
+        }
+
+        freeImposterTextures.Clear();
+    }
+
+    public bool getPreloadLock(Imposter imposter)
+    {
+        if (preloadCounter++ < preloadRate) return true;
+        else return false;
+    }
+
+    void Clear()
+    {
+        foreach(Imposter imposter in imposters)
+        {
+            if (imposter != null)
+            {
+                Destroy(imposter.gameObject);
+            }
+        }
+
+        foreach (ImposterTexture imposterTexture in imposterTextures)
+        {
+            textureMemory -= imposterTexture.getMemoryAmount();
+            Destroy(imposterTexture.texture);
+        }
+
+        imposters.Clear();
+        imposterTextures.Clear();
+        freeImposterTextures.Clear();
+    }
+}
