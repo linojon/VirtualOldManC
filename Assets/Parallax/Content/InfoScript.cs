6795f36990d0c7e29e82fdf616499c54c937e132
Krystian Babilinski <kbabilinski@gmail.com>
2015-10-19 13:38:45 -0400

    Added Jagged Variation
    Added a new Model, cleaned up the project

---
+++ Assets/Parallax/Content/InfoScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InfoScript : MonoBehaviour {
+
+    public GameObject Head;
+    public Text Distance;
+    public Text Parts;
+    public Text Scale;
+
+	// Use this for initialization
+	void Start () {
+        float _distance = Vector3.Distance(Camera.main.transform.position, Head.transform.position);
+        Distance.text = "Distance: " + _distance;
+
+        int layers = 0;
+        foreach(Transform trans in Head.GetComponentsInChildren<Transform>())
+        {
+            layers++;
+        }
+        Parts.text = "Parts: " + layers;
+
+        Scale.text = "Scale: " + Head.transform.localScale.z;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
